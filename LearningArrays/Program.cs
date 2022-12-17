731e22b149df9ef4749fb0deada5a33d9a33b8da
Linases <lina.tauraite@gmail.com>
2022-12-17 20:58:21 +0200

    Implemented code

---
+++ LearningArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valuearray = new string[5] { "First", "Second", "Third", "Thourth", "Fifth" };
+            
+            for (int i = 0; i < valuearray.Length; i++)
+            {
+                string word = valuearray[i];
+                Console.WriteLine(word);
+            }
+
+
+            int[] intArray = new int[20];
+       
+            for (int i = intArray.Length - 1; i >= 0; i--)
+            {
+                intArray[i] = i;
+                Console.WriteLine(intArray[i]);
+            }
+        }
+                 
+        }
+    }
+
